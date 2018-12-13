943a967410d8d0d00393869caafe3cfeed343819
nimrodh01 <nimrod.haller@gmail.com>
2018-12-13 08:14:16 +0200

    adding ng-init to load communities array upon load

--- app/login/loginCtrl.js
+++ app/login/loginCtrl.js
@@ -1,7 +1,7 @@
-app.controller("loginCtrl", function ($scope, $location, user) {
-    
-    $scope.email = "nimrod.haller@gmail.com";
-    $scope.pwd = "123";
+app.controller("loginCtrl", function ($scope, $location, communities, user) {
+
+    // $scope.email = "nimrod.haller@gmail.com";
+    // $scope.pwd = "123";
 
     $scope.invalidLogin = false;
 
@@ -10,11 +10,42 @@ app.controller("loginCtrl", function ($scope, $location, user) {
 
         user.login($scope.email, $scope.pwd).then(function () {
             $('#loginModal').modal('hide');
+            $scope.communityNameById();
             $location.path("/")
         }, function (error) {
             $scope.invalidLogin = true;
         })
     }
+
+    $scope.communities = [];
+    $scope.getCommunities = function () {
+        communities.getAllCommunities().then(function (communities) {
+            $scope.communities = communities
+        })
+
+    }
+
+    // $scope.communityName = "Community";
+    $scope.communityNameById = function () {
+        if ($scope.isUserLoggedIn()) {
+            var communityId = user.getActiveUser().communityId;
+            $scope.communityName = communities.getCommunityNameById(communityId);
+        }
+    }
+
+
+
+
+
+
+    // Remember Me
+    //     if ($scope.rememberMe && $scope.email && $scope.pwd)
+    //     {
+    //     $scope.email = $scope.email;
+    //     $scope.pwd = $scope.pwd;
+    //     $scope.login
+    // }
+
 
 
 }); 
